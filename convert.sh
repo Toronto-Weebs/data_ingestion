fff46ba60ab926e23b7975ba39dc4536b65f7ded
Ian Yu <63731742+ianyu93@users.noreply.github.com>
2022-07-06 18:54:19 -0400

    Update convert.sh
    Fixed output dir

--- convert.sh
+++ convert.sh
@@ -5,7 +5,7 @@ OUTPUT_DIR=$2
 ffmpeg -i sample.mp4 -vf "select='eq(pict_type,I)'" -vsync vfr out-%02d.jpeg
 
 for video in INPUT_DIR/*; do
-    mkdir -p "$OUTPUT_IDR/$video" # create a directory for each video
-    VIDEO_DIR="$OUTPUT_IDR/$video" # set the directory to the video
-    ffmpeg -i $video -vf "select='eq(pict_type,I)'" -vsync vfr "$videVIDEO_DIR/out-%02d.jpeg" # extract the IDR frames
+    mkdir -p $OUTPUT_IDR/$video # create a directory for each video
+    VIDEO_DIR=$OUTPUT_IDR/$video # set the directory to the video
+    ffmpeg -i $video -vf "select='eq(pict_type,I)'" -vsync vfr "${VIDEO_DIR}/out-%02d.jpeg" # extract the IDR frames
 done
